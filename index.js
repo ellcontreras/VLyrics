c63a170a6e7690ebc565746ac974c6c4e9ddc2cd
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-04-13 13:32:11 -0500

    Installed body-parser

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 "use strict";
 
 const express = require("express");
-
 const app = express();
+
+const bodyParser = require("body-parser");
+const db = require("./database");
 
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hola mundo");
-});
+const lyricController = require("./controllers/lyricController");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.get("/:name", (req, res) => {
+//   db.get("lyrics")
+//     .push({
+//       id: db.get("lyricsId").value(),
+//       songName: "runaway",
+//       songLyric: "ascasdc"
+//     })
+//     .write();
+
+//   db.update("lyricsId", n => n + 1).write();
+
+//   res.send(`Hola ${req.params.name}`);
+// });
+
+app.post("/", lyricController.addLyric);
 
 app.listen(PORT, () => console.log(`server running on :${PORT}`));
