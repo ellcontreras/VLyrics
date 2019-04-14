f0b631cd856242bc4ed92bc7e608843a58eda2b8
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-04-14 12:31:51 -0500

    Added allow-methods CORS

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
