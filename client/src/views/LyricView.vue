5dd000313f724cd0a3e2591bee218f7fa1a5ec36
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-04-14 12:33:32 -0500

    (Adding) button to delete a lyric and call to vuex

--- client/src/views/LyricView.vue
+++ client/src/views/LyricView.vue
@@ -2,15 +2,24 @@
   <div>
     <h1 class="text-center">{{ lyric.songName }}</h1>
     <p class="text-center mt-6">{{ lyric.songLyric }}</p>
+    <button class="bg-red-light text-white hover:bg-red p-2 rounded" @click="deleteLyric">Delete</button>
   </div>
 </template>
 
 <script>
+import { RepositoryFactory } from "../repository/RepositoryFactory";
+const LyricsRepository = RepositoryFactory.get("lyrics");
+
 export default {
   name: "LyricView",
   computed: {
     lyric() {
       return this.$store.getters.lyricById(this.$route.params.id);
+    }
+  },
+  methods: {
+    async deleteLyric() {
+      this.$store.dispatch("deleteLyric", this.$route.params.id);
     }
   }
 };
